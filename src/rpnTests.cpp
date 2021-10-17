fcb733a56adf2b388d5632937cb4535ecf0afae6
Michał Orynicz <morynicz@gmail.com>
2021-10-17 23:05:30 +0200

    Add test for handling 3 2 - -> 1

--- src/rpnTests.cpp
+++ src/rpnTests.cpp
@@ -21,7 +21,30 @@ TEST(TestRpn, twoNumbersWithoutOperatorCauseInvalidInputException)
     EXPECT_THROW(rpn::calculate("2 2"), rpn::InvalidInputException);
 }
 
-TEST(TestRpn, twoPlusTwoReturnsFour)
+struct OperationAndResult
 {
-    EXPECT_EQ(rpn::calculate("2 2 +"), 4);
+    const std::string operation;
+    const rpn::ResultType expectedResult;
+};
+
+std::ostream& operator<<(std::ostream& str, const OperationAndResult& op)
+{
+    return str << "Operation: " << op.operation
+               << ", expectedResult: " << op.expectedResult;
 }
+
+struct TestRpnNonExceptionCases
+    : public testing::TestWithParam<OperationAndResult>
+{
+};
+
+TEST_P(TestRpnNonExceptionCases, calculatedOperationReturnsExpectedResult)
+{
+    auto params = GetParam();
+    EXPECT_EQ(rpn::calculate(params.operation), params.expectedResult);
+}
+
+INSTANTIATE_TEST_SUITE_P(RpnTests,
+                         TestRpnNonExceptionCases,
+                         testing::Values(OperationAndResult{ "2 2 +", 4 },
+                                         OperationAndResult{ "3 2 -", 1 }));
