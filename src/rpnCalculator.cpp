adbf249b81488b8884d17d6d57cc2b53413974ec
Michał Orynicz <morynicz@gmail.com>
2021-10-17 21:09:11 +0200

    Pass test for two numbers without operator trhowing invalid input

--- src/rpnCalculator.cpp
+++ src/rpnCalculator.cpp
@@ -1,23 +1,38 @@
 #include "rpnCalculator.hpp"
+#include <list>
 #include <sstream>
 
 namespace rpn
 {
 ResultType calculate(const std::string& input)
 {
+    std::list<ResultType> stack;
     if (input.size() == 0)
     {
         return 0;
     }
-    auto space = input.find_first_of(' ');
-    try
+    auto start = 0;
+    do
     {
-        return std::stoi(input.substr(0, space));
-    }
-    catch (const std::invalid_argument)
+        auto end = input.find(' ', start);
+        try
+        {
+            stack.push_front(std::stoi(input.substr(start, end)));
+            start = end + 1;
+            if (end == std::string::npos)
+            {
+                break;
+            }
+        }
+        catch (const std::invalid_argument)
+        {
+            throw InvalidInputException("non numeric input");
+        }
+    } while (true);
+    if (stack.size() != 1)
     {
-        throw InvalidInputException("non numeric input");
+        throw InvalidInputException("malformed operation");
     }
-    return 0;
+    return stack.front();
 }
 }
