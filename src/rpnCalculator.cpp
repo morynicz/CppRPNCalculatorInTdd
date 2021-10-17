bd725b2a40067b6c88dfc586021b09789f77213c
Michał Orynicz <morynicz@gmail.com>
2021-10-17 23:31:54 +0200

    Cut calculateResult to couple smaller functions

--- src/rpnCalculator.cpp
+++ src/rpnCalculator.cpp
@@ -1,44 +1,60 @@
 #include "rpnCalculator.hpp"
 #include <boost/range/adaptor/tokenized.hpp>
+#include <functional>
 #include <list>
+#include <map>
 
 namespace rpn
 {
 namespace
 {
+using Stack = std::list<ResultType>;
+using Operator = std::function<ResultType(ResultType, ResultType)>;
+
+std::map<char, Operator> operators{
+    { '+', [](ResultType lhs, ResultType rhs) { return lhs + rhs; } },
+    { '-', [](ResultType lhs, ResultType rhs) { return lhs - rhs; } }
+};
+
+std::tuple<ResultType, ResultType> pop2(Stack& stack)
+{
+    auto arg1 = stack.front();
+    stack.pop_front();
+    auto arg2 = stack.front();
+    stack.pop_front();
+    return { arg1, arg2 };
+}
+
+void applyOperation(Stack& stack, const Operator& op)
+{
+    const auto [arg1, arg2] = pop2(stack);
+    stack.push_front(op(arg2, arg1));
+}
+
+void storeNumber(Stack& stack, const std::string number)
+try
+{
+    stack.push_front(std::stoi(number));
+}
+catch (const std::invalid_argument)
+{
+    throw InvalidInputException("non numeric input");
+}
+
 template<typename T>
 auto calculateResult(const T& tokens)
 {
-    std::list<ResultType> stack;
+    Stack stack;
 
-    for (const auto& token : tokens)
+    for (const std::string& token : tokens)
     {
-        if ("+" == token)
+        if (const auto operation = operators.find(token.back());
+            operation != operators.end())
         {
-            auto arg1 = stack.front();
-            stack.pop_front();
-            auto arg2 = stack.front();
-            stack.pop_front();
-            stack.push_front(arg1 + arg2);
+            applyOperation(stack, operation->second);
             continue;
         }
-        if ("-" == token)
-        {
-            auto arg1 = stack.front();
-            stack.pop_front();
-            auto arg2 = stack.front();
-            stack.pop_front();
-            stack.push_front(arg2 - arg1);
-            continue;
-        }
-        try
-        {
-            stack.push_front(std::stoi(token));
-        }
-        catch (const std::invalid_argument)
-        {
-            throw InvalidInputException("non numeric input");
-        }
+        storeNumber(stack, token);
     }
     return stack;
 }
