9925ee1956dff5e5c000494ee92dfd64e6461e46
Michał Orynicz <morynicz@gmail.com>
2021-10-17 20:41:08 +0200

    Implement conversion from string to number

--- src/rpnCalculator.cpp
+++ src/rpnCalculator.cpp
@@ -1,10 +1,20 @@
 #include "rpnCalculator.hpp"
+#include <sstream>
 
 namespace rpn
 {
 ResultType calculate(const std::string& input)
 {
-    if(input.size())
+    if (input.size() == 0)
+    {
+        return 0;
+    }
+    auto space = input.find_first_of(' ');
+    try
+    {
+        return std::stoi(input.substr(0, space));
+    }
+    catch (const std::invalid_argument)
     {
         throw InvalidInputException("non numeric input");
     }
